b036cd8da06aae1d39bafd6d2863b93eda74dd1c
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-04-22 21:35:32 +0700

    add bson test

--- bb02/src/main.rs
+++ bb02/src/main.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 struct Move {
     direction: Direction,
-    steps: u32,
+    steps: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,44 +15,65 @@ enum Direction {
 }
 
 fn main() {
-    let move_a = Move {
+    let mv = Move {
         direction: Direction::Up,
         steps: 3,
     };
-    println!("`move_a` original: {:?}", move_a);
+    println!("`mv` original: {:?}", mv);
 
     // JSON format
     {
-        let json_file = std::fs::File::create("./test.json").unwrap();
-        serde_json::to_writer(json_file, &move_a).unwrap();
+        let f_json = std::fs::File::create("./test.json").unwrap();
+        serde_json::to_writer(f_json, &mv).unwrap();
 
-        let json_file = std::fs::File::open("./test.json").unwrap();
-        let move_a: Move = serde_json::from_reader(json_file).unwrap();
-        println!("\n`move_a` from JSON: {:?}", move_a);
+        let f_json = std::fs::File::open("./test.json").unwrap();
+        let mv: Move = serde_json::from_reader(f_json).unwrap();
+        println!("\n`mv` from JSON: {:?}", mv);
 
-        let move_a_json = serde_json::to_vec(&move_a).unwrap();
-        println!("`move_a` JSON to vec: {:?}", &move_a_json);
+        let mv_json = serde_json::to_vec(&mv).unwrap();
+        println!("`mv` JSON to vec: {:?}", &mv_json);
         println!(
-            "`move_a` JSON to string: {:?}",
-            String::from_utf8_lossy(&move_a_json)
+            "`mv` JSON to string: {:?}",
+            String::from_utf8_lossy(&mv_json)
         );
     }
 
     // RON format
     {
-        let ron_file = std::fs::File::create("./test.ron").unwrap();
-        ron::ser::to_writer(ron_file, &move_a).unwrap();
+        let f_ron = std::fs::File::create("./test.ron").unwrap();
+        ron::ser::to_writer(f_ron, &mv).unwrap();
 
-        let ron_file = std::fs::File::open("./test.ron").unwrap();
-        let move_a: Move = ron::de::from_reader(ron_file).unwrap();
-        println!("\n`move_a` from RON: {:?}", move_a);
+        let f_ron = std::fs::File::open("./test.ron").unwrap();
+        let mv: Move = ron::de::from_reader(f_ron).unwrap();
+        println!("\n`mv` from RON: {:?}", mv);
 
-        let mut move_a_ron = Vec::new();
-        ron::ser::to_writer(&mut move_a_ron, &move_a).unwrap();
-        println!("`move_a` RON to vec: {:?}", &move_a_ron);
+        let mut mv_ron = Vec::new();
+        ron::ser::to_writer(&mut mv_ron, &mv).unwrap();
+        println!("`mv` RON to vec: {:?}", &mv_ron);
         println!(
-            "`move_a` RON to string: `{:?}",
-            String::from_utf8_lossy(&move_a_ron)
+            "`mv` RON to string: `{:?}",
+            String::from_utf8_lossy(&mv_ron)
+        );
+    }
+
+    // BSON format
+    {
+        let mut f_bson = std::fs::File::create("./test.bson").unwrap();
+        let mv_bson_doc = bson::to_document(&mv).unwrap();
+        mv_bson_doc.to_writer(&mut f_bson).unwrap();
+
+        let mut f_bson = std::fs::File::open("./test.bson").unwrap();
+        let mv_bson_doc = bson::Document::from_reader(&mut f_bson).unwrap();
+        let mv: Move = bson::from_document(mv_bson_doc).unwrap();
+        println!("\n`mv` from BSON: {:?}", mv);
+
+        let mut mv_bson_vec = Vec::new();
+        let mv_bson_doc = bson::to_document(&mv).unwrap();
+        mv_bson_doc.to_writer(&mut mv_bson_vec).unwrap();
+        println!("`mv` BSON to vec: {:?}", &mv_bson_vec);
+        println!(
+            "`mv` BSON to string: `{:?}",
+            String::from_utf8_lossy(&mv_bson_vec)
         );
     }
 }
