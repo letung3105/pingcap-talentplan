9a75c7990c5dcf2f6d4ea76c575893790e8d54b5
letung3105 <15053897+letung3105@users.noreply.github.com>
2021-04-26 18:22:14 +0700

    use tcp stream

--- pna/bb03/src/bin/redisc.rs
+++ pna/bb03/src/bin/redisc.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("a custom Redis client")
+use std::io::{BufWriter, BufReader};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr_local = SocketAddr::from(bb03::TEST_ADDR);
+    let stream = TcpStream::connect(addr_local)?;
+    Ok(())
 }
--- pna/bb03/src/bin/redisd.rs
+++ pna/bb03/src/bin/redisd.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("a custom Redis daemon")
+use std::net::{SocketAddr, TcpListener};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr_local = SocketAddr::from(bb03::TEST_ADDR);
+    let listener = TcpListener::bind(addr_local)?;
+    match listener.accept() {
+        Ok((_stream, addr_remote)) => {
+            println!("Establish new connection with {}", addr_remote);
+        }
+        Err(err) => {
+            eprintln!("Error while accepting new TCP connection {}", err);
+            std::process::exit(1);
+        }
+    }
+    Ok(())
 }
