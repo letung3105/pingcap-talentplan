aedca932b4fa82772964e9c4af04e7cff442f65a
letung3105 <15053897+letung3105@users.noreply.github.com>
2021-05-26 14:47:01 +0700

    refactor

--- pna/kvs/benches/benches.rs
+++ pna/kvs/benches/benches.rs
@@ -14,121 +14,109 @@ criterion_group! {
 }
 
 mod engines {
+    use kvs::{KvStore, KvsEngine, SledKvsEngine};
+
     use super::*;
 
-    // NOTE: Running the benchmarks with a maximum key/value size of 100000 as stated in the project
-    // requirements will cause sled to hang, this might be because of we are flushing the in-memory
-    // data on every write/remove
-    const MAX_KEY_SIZE: usize = 100000;
-    const MAX_VAL_SIZE: usize = 100000;
-
     pub fn write(c: &mut Criterion) {
-        let mut rng = StdRng::from_seed([0u8; 32]);
-        let (kv_pairs, bytes) = prebuilt_kv_pairs(&mut rng, 1000, MAX_KEY_SIZE, MAX_VAL_SIZE);
-
-        let mut g = c.benchmark_group("engines::write");
-        g.throughput(Throughput::Bytes(bytes as u64));
-        g.bench_with_input("kvs", &(KvsEngineBackend::Kvs, &kv_pairs), write_bench);
-        g.bench_with_input("sled", &(KvsEngineBackend::Sled, &kv_pairs), write_bench);
-        g.finish();
-    }
-
-    fn write_bench(
-        b: &mut Bencher,
-        &(engine_backend, kv_pairs): &(KvsEngineBackend, &Vec<(String, String)>),
-    ) {
-        b.iter_batched(
-            || {
+        let bench =
+            |b: &mut Bencher,
+             &(engine_backend, key_size, val_size): &(KvsEngineBackend, usize, usize)| {
                 let tmpdir = TempDir::new().unwrap();
-                let kv_store = kvs::open(tmpdir.path(), engine_backend).unwrap();
-                // collect tmpdir so that it is only going to be dropped when to benchmarch ends
-                (kv_store, kv_pairs.clone(), tmpdir)
-            },
-            |(mut kv_store, kv_pairs, _tmpdir)| {
-                kv_pairs
-                    .into_iter()
-                    .for_each(|(k, v)| kv_store.set(black_box(k), black_box(v)).unwrap());
-            },
-            BatchSize::LargeInput,
-        );
+                let mut kvs_engine: Box<dyn KvsEngine> = match engine_backend {
+                    KvsEngineBackend::Kvs => Box::new(KvStore::open(tmpdir.path()).unwrap()),
+                    KvsEngineBackend::Sled => Box::new(SledKvsEngine::open(tmpdir.path()).unwrap()),
+                };
+                let mut rng = StdRng::from_seed([0u8; 32]);
+
+                b.iter_batched(
+                    || rand_key_value(&mut rng, key_size, val_size),
+                    |(k, v)| {
+                        kvs_engine.set(black_box(k), black_box(v)).unwrap();
+                    },
+                    BatchSize::SmallInput,
+                );
+            };
+
+        for key_size in [10_usize, 128, 256, 512].iter() {
+            for val_size in [0_usize, 10, 128, 256, 512, 1024, 2048, 4096, 8192].iter() {
+                let mut g =
+                    c.benchmark_group(format!("write (key/val size {}B/{}B)", key_size, val_size));
+                g.throughput(Throughput::Bytes((key_size + val_size) as u64));
+                g.bench_with_input("kvs", &(KvsEngineBackend::Kvs, *key_size, *val_size), bench);
+                g.bench_with_input("sled", &(KvsEngineBackend::Sled, *key_size, *val_size), bench);
+                g.finish();
+            }
+        }
     }
 
     pub fn read(c: &mut Criterion) {
-        let mut rng = StdRng::from_seed([0u8; 32]);
-        let (kv_pairs, bytes) = prebuilt_kv_pairs(&mut rng, 1000, MAX_KEY_SIZE, MAX_VAL_SIZE);
+        let bench =
+            |b: &mut Bencher,
+             &(engine_backend, kv_pairs): &(KvsEngineBackend, &Vec<(String, String)>)| {
+                let tmpdir = TempDir::new().unwrap();
+                let mut kvs_engine: Box<dyn KvsEngine> = match engine_backend {
+                    KvsEngineBackend::Kvs => Box::new(KvStore::open(tmpdir.path()).unwrap()),
+                    KvsEngineBackend::Sled => Box::new(SledKvsEngine::open(tmpdir.path()).unwrap()),
+                };
+                let mut rng = StdRng::from_seed([0u8; 32]);
+                kv_pairs
+                    .iter()
+                    .for_each(|(k, v)| kvs_engine.set(k.clone(), v.clone()).unwrap());
 
-        let mut g = c.benchmark_group("engines::read");
-        g.throughput(Throughput::Bytes(bytes as u64));
-        g.bench_with_input("kvs", &(KvsEngineBackend::Kvs, &kv_pairs), read_bench);
-        g.bench_with_input("sled", &(KvsEngineBackend::Sled, &kv_pairs), read_bench);
-        g.finish();
-    }
+                b.iter_batched(
+                    || kv_pairs.choose(&mut rng).cloned().unwrap(),
+                    |(k, v)| {
+                        assert_eq!(Some(v), kvs_engine.get(black_box(k)).unwrap());
+                    },
+                    BatchSize::SmallInput,
+                );
+            };
 
-    fn read_bench(
-        b: &mut Bencher,
-        &(engine_backend, kv_pairs): &(KvsEngineBackend, &Vec<(String, String)>),
-    ) {
-        let tmpdir = TempDir::new().unwrap();
-        let mut kv_store = kvs::open(tmpdir.path(), engine_backend).unwrap();
-        kv_pairs
-            .iter()
-            .for_each(|(k, v)| kv_store.set(k.clone(), v.clone()).unwrap());
+        for key_size in [10_usize, 128, 256, 512].iter() {
+            for val_size in [0_usize, 10, 128, 256, 512, 1024, 2048, 4096, 8192].iter() {
+                let mut rng = StdRng::from_seed([0u8; 32]);
+                let kv_pairs = prebuilt_kv_pairs(&mut rng, 1000, *key_size, *val_size);
 
-        let mut shuffling_rng = StdRng::from_seed([0u8; 32]);
-        b.iter_batched(
-            || {
-                let mut shuffled_kv_pairs = kv_pairs.clone();
-                shuffled_kv_pairs.shuffle(&mut shuffling_rng);
-                shuffled_kv_pairs
-            },
-            |shuffled_kv_pairs| {
-                shuffled_kv_pairs
-                    .into_iter()
-                    .for_each(|(k, v)| assert_eq!(Some(v), kv_store.get(black_box(k)).unwrap()))
-            },
-            BatchSize::LargeInput,
-        );
+                let mut g =
+                    c.benchmark_group(format!("read (key/val size {}B/{}B)", key_size, val_size));
+                g.throughput(Throughput::Bytes((*key_size + *val_size) as u64));
+                g.bench_with_input("kvs", &(KvsEngineBackend::Kvs, &kv_pairs), bench);
+                g.bench_with_input("sled", &(KvsEngineBackend::Sled, &kv_pairs), bench);
+                g.finish();
+            }
+        }
     }
 
     fn prebuilt_kv_pairs<R>(
         rng: &mut R,
         size: usize,
-        max_key_size: usize,
-        max_val_size: usize,
-    ) -> (Vec<(String, String)>, usize)
+        key_size: usize,
+        val_size: usize,
+    ) -> Vec<(String, String)>
     where
         R: Rng,
     {
-        let mut bytes = 0;
-        let kv_pairs = (0..size)
+        (0..size)
             .into_iter()
-            .map(|_| {
-                let (k, v) = rand_key_value(rng, max_key_size, max_val_size);
-                bytes += k.as_bytes().len() + v.as_bytes().len();
-                (k, v)
-            })
-            .collect();
-        (kv_pairs, bytes)
+            .map(|_| rand_key_value(rng, key_size, val_size))
+            .collect()
     }
 
-    fn rand_key_value<R>(rng: &mut R, max_key_size: usize, max_val_size: usize) -> (String, String)
+    fn rand_key_value<R>(rng: &mut R, key_size: usize, val_size: usize) -> (String, String)
     where
         R: Rng,
     {
-        let k_size = rng.gen_range(1..=max_key_size);
-        let k: String = rng
+        let key: String = rng
             .sample_iter(Alphanumeric)
-            .take(k_size)
+            .take(key_size)
             .map(char::from)
             .collect();
-
-        let v_size = rng.gen_range(1..=max_val_size);
-        let v: String = rng
+        let val: String = rng
             .sample_iter(Alphanumeric)
-            .take(v_size)
+            .take(val_size)
             .map(char::from)
             .collect();
-
-        (k, v)
+        (key, val)
     }
 }
