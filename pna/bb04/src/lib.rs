d78d64a934b4794befe03bd46b43ee6668e9aae4
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-05-31 20:08:14 +0700

    comments

--- pna/bb04/src/lib.rs
+++ pna/bb04/src/lib.rs
@@ -1,3 +1,7 @@
+//! A simple thread pool implementation
+
+#![warn(missing_docs)]
+
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Condvar, Mutex};
@@ -5,21 +9,25 @@ use std::thread::JoinHandle;
 
 type Thunk<'a> = Box<dyn FnOnce() + Send + 'a>;
 
+/// A thread pool where tasks can be queued and wait for execution, this structure allows multiple
+/// threads to manage the same thread pool
 pub struct ThreadPool {
     jobs_tx: Sender<Thunk<'static>>,
     context: Arc<Context>,
 }
 
 impl ThreadPool {
-    pub fn new(threads: u32) -> Result<Self, Box<dyn std::error::Error>> {
+    /// Create a new thread pool that keeps `threads` number of threads running at the same time
+    pub fn new(threads: u32) -> Self {
         let (jobs_tx, jobs_rx) = mpsc::channel();
-        let context = Arc::new(Context::new(jobs_rx));
+        let context = Context::new(jobs_rx);
         (0..threads).for_each(|_| {
             spawn(context.clone());
         });
-        Ok(Self { jobs_tx, context })
+        Self { jobs_tx, context }
     }
 
+    /// Queue a task for execution by one of the thread in the pool
     pub fn exec<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
@@ -30,6 +38,7 @@ impl ThreadPool {
             .expect("Could not queue job for execution.")
     }
 
+    /// Blocks the calling thread until there is no more queued and running task
     pub fn join(&self) {
         if !self.context.has_work() {
             return;
@@ -64,7 +73,9 @@ impl Clone for ThreadPool {
     }
 }
 
-pub struct Context {
+/// A context that is shared by all threads in the thread pool. Inter-thread communications and
+/// sychronizations are performed through this structure
+struct Context {
     jobs_rx: Mutex<Receiver<Thunk<'static>>>,
     count_queued: AtomicUsize,
     count_active: AtomicUsize,
@@ -74,21 +85,26 @@ pub struct Context {
 }
 
 impl Context {
-    pub fn new(jobs_rx: Receiver<Thunk<'static>>) -> Self {
-        Self {
+    /// Create a new thread context which wraps around the given jobs receiver, the structure is
+    /// wrapped by an [`Arc`] so that it can be shared to multiple threads
+    fn new(jobs_rx: Receiver<Thunk<'static>>) -> Arc<Self> {
+        Arc::new(Self {
             jobs_rx: Mutex::new(jobs_rx),
             count_queued: AtomicUsize::new(0),
             count_active: AtomicUsize::new(0),
             join_generation: AtomicUsize::new(0),
             join_lock: Mutex::new(()),
             join_condvar: Condvar::new(),
-        }
+        })
     }
-    pub fn has_work(&self) -> bool {
+
+    /// Return true if there is no running task and no task is waiting for execution
+    fn has_work(&self) -> bool {
         self.count_queued.load(Ordering::SeqCst) > 0 || self.count_active.load(Ordering::SeqCst) > 0
     }
 
-    pub fn notify_all_join(&self) {
+    /// Signal that a task has done its execution
+    fn join_notify_all(&self) {
         if !self.has_work() {
             *self
                 .join_lock
@@ -105,14 +121,14 @@ struct Sentinel<'a> {
 }
 
 impl<'a> Sentinel<'a> {
-    pub fn new(context: &'a Arc<Context>) -> Self {
+    fn new(context: &'a Arc<Context>) -> Self {
         Self {
             context,
             active: true,
         }
     }
 
-    pub fn stop(mut self) {
+    fn stop(mut self) {
         self.active = false;
     }
 }
@@ -121,7 +137,7 @@ impl<'a> Drop for Sentinel<'a> {
     fn drop(&mut self) {
         if self.active {
             self.context.count_active.fetch_sub(1, Ordering::SeqCst);
-            self.context.notify_all_join();
+            self.context.join_notify_all();
             spawn(self.context.clone());
         }
     }
@@ -149,7 +165,7 @@ fn spawn(context: Arc<Context>) -> JoinHandle<()> {
             context.count_queued.fetch_sub(1, Ordering::SeqCst);
             job();
             context.count_active.fetch_sub(1, Ordering::SeqCst);
-            context.notify_all_join();
+            context.join_notify_all();
         }
         sentinel.stop();
     })
