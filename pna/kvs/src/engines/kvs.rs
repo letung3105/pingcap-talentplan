7ffe9eab5052e640f0f64d56df14575b89330a53
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-06-08 00:13:41 +0700

    use read/write contexts

--- pna/kvs/src/engines/kvs.rs
+++ pna/kvs/src/engines/kvs.rs
@@ -2,8 +2,9 @@
 
 use crate::{Error, ErrorKind, KvsEngine, Result};
 use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
 use std::collections::BTreeMap;
-use std::collections::HashMap;
+
 use std::ffi::OsStr;
 use std::fs::{self, File, OpenOptions};
 use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
@@ -48,15 +49,15 @@ const GARBAGE_THRESHOLD: u64 = 4 * 1024 * 1024;
 ///     Ok(())
 /// }
 /// ```
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct KvStore {
     // NOTE: Breaking up the lock
     //
     // # Requirements
     // - Read from index and from disk on multiple threads at a time
-    // - Write command to disk while maintaining the index
+    // - Write log to disk while maintaining the index
     // - Read in parallel with write, i.e., readers will always see a consistent state
-    //   - Maintaining an invariant that the index always points to a valid command in the log
+    //   - Maintaining an invariant that the index always points to a valid entry in the log
     //   - Maintaining other invariants for bookkeeping state
     // - Periodically compact disk's data, while maintaining invariants for readers
     //
@@ -69,7 +70,17 @@ pub struct KvStore {
     // - Use specialized concurrent data structure
     // - Postpone cleanup until later
     // - Share flags and counters with atomics
-    context: Arc<Mutex<Context>>,
+    w_context: Arc<Mutex<WriteContext>>,
+    r_context: ReadContext,
+}
+
+impl Clone for KvStore {
+    fn clone(&self) -> Self {
+        Self {
+            w_context: Arc::clone(&self.w_context),
+            r_context: self.r_context.clone(),
+        }
+    }
 }
 
 impl KvStore {
@@ -78,36 +89,42 @@ impl KvStore {
     where
         P: AsRef<Path>,
     {
-        let prev_epochs = previous_epochs(&path)?;
-        let active_epoch = prev_epochs.last().map(|&e| e + 1).unwrap_or_default();
+        let prev_gens = previous_gens(&path)?;
+        let gen = prev_gens.last().map(|&e| e + 1).unwrap_or_default();
 
         // go through all log files, rebuild the index, and keep the handle to each log for later access
         let mut garbage = 0;
-        let mut readers = HashMap::new();
-        let mut index_map = BTreeMap::new();
-        for prev_epoch in prev_epochs {
-            let prev_log_path = path.as_ref().join(format!("epoch-{}.log", prev_epoch));
-            let prev_log = OpenOptions::new().read(true).open(prev_log_path)?;
-            let mut reader = BufSeekReader::new(prev_log)?;
-
-            garbage += build_index(&mut reader, &mut index_map, prev_epoch)?;
-            readers.insert(prev_epoch, reader);
+        let mut index = BTreeMap::new();
+        let mut readers = BTreeMap::new();
+        for prev_gen in prev_gens {
+            let mut reader = open_log(&path, prev_gen)?;
+            garbage += build_index(&mut reader, &mut index, prev_gen)?;
+            readers.insert(prev_gen, reader);
         }
         // create a new log file for this instance, taking a write handle and a read handle for it
-        let (writer, reader) = create_log(&path, active_epoch)?;
-        readers.insert(active_epoch, reader);
+        let (writer, reader) = create_log(&path, gen)?;
+        readers.insert(gen, reader);
 
-        let context = Context {
-            active_path: path.as_ref().to_path_buf(),
-            active_epoch,
-            garbage,
+        let path = Arc::new(path.as_ref().to_path_buf());
+        let index = Arc::new(RwLock::new(index));
+
+        let w_context = WriteContext {
+            path: Arc::clone(&path),
+            index: Arc::clone(&index),
             writer,
-            readers,
-            index_map,
+            gen,
+            garbage,
+        };
+
+        let r_context = ReadContext {
+            path: Arc::clone(&path),
+            index: Arc::clone(&index),
+            readers: RefCell::new(readers),
         };
 
         Ok(Self {
-            context: Arc::new(Mutex::new(context)),
+            w_context: Arc::new(Mutex::new(w_context)),
+            r_context,
         })
     }
 }
@@ -117,8 +134,7 @@ impl KvsEngine for KvStore {
     ///
     /// Error from I/O operations and serialization/deserialization operations will be propagated.
     fn set(&self, key: String, val: String) -> Result<()> {
-        let mut context = self.context.lock().unwrap();
-        context.set(key, val)
+        self.w_context.lock().unwrap().set(key, val)
     }
 
     /// Returns the value of a key, if the key exists. Otherwise, returns `None`.
@@ -127,8 +143,7 @@ impl KvsEngine for KvStore {
     ///
     /// Error from I/O operations will be propagated.
     fn get(&self, key: String) -> Result<Option<String>> {
-        let mut context = self.context.lock().unwrap();
-        context.get(key)
+        self.r_context.get(key)
     }
 
     /// Removes a key.
@@ -138,187 +153,34 @@ impl KvsEngine for KvStore {
     /// Error from I/O operations will be propagated. If the key doesn't exist returns a
     /// `KeyNotFound` error.
     fn remove(&self, key: String) -> Result<()> {
-        let mut context = self.context.lock().unwrap();
-        context.remove(key)
-    }
-}
-
-#[derive(Debug)]
-struct Context {
-    active_path: PathBuf,
-    active_epoch: u64,
-    garbage: u64,
-    writer: BufSeekWriter<File>,
-    readers: HashMap<u64, BufSeekReader<File>>,
-    index_map: BTreeMap<String, LogIndex>,
-}
-
-impl Context {
-    fn set(&mut self, key: String, val: String) -> Result<()> {
-        /*
-            A `LogEntry` for `Set` command is written to a sequential log on disk, then the log
-            pointer (file position) is stored in an in-memory index from key to pointer.
-        */
-        let log_entry = LogEntry::Set(key.clone(), val);
-
-        let pos = self.writer.pos;
-        bincode::serialize_into(&mut self.writer, &log_entry)?;
-        self.writer.flush()?;
-        let len = self.writer.pos - pos;
-
-        let index = LogIndex {
-            gen: self.active_epoch,
-            pos,
-            len,
-        };
-        if let Some(prev_index) = self.index_map.insert(key, index) {
-            self.garbage += prev_index.len;
-            if self.garbage > GARBAGE_THRESHOLD {
-                self.merge()?;
-            }
-        };
-
-        Ok(())
-    }
-
-    fn get(&mut self, key: String) -> Result<Option<String>> {
-        /*
-            When retrieving a value for a key, the store searches for the key in the index. If
-            found an index, loads the log entry at the corresponding log pointer, evaluates the
-            contained command, and returns the result.
-        */
-        let get_result = self.index_map.get(&key).cloned();
-        match get_result {
-            Some(index) => match self.readers.get_mut(&index.gen) {
-                Some(reader) => {
-                    reader.seek(SeekFrom::Start(index.pos))?;
-                    match bincode::deserialize_from(reader)? {
-                        LogEntry::Set(_, value) => Ok(Some(value)),
-                        _ => Err(Error::new(
-                            ErrorKind::CorruptedLog,
-                            "Expecting a log entry for a set operation",
-                        )),
-                    }
-                }
-                None => Err(Error::new(
-                    ErrorKind::CorruptedIndex,
-                    format!("Could not get reader for epoch #{}", index.gen),
-                )),
-            },
-            None => Ok(None),
-        }
-    }
-
-    fn remove(&mut self, key: String) -> Result<()> {
-        /*
-            When removing a key, the store searches for the key in the index. If an index is found,
-            a log entry for the remove command is written to the on-disk sequential log, and the key
-            is then removed from the index.
-        */
-
-        if !self.index_map.contains_key(&key) {
-            return Err(Error::new(
-                ErrorKind::KeyNotFound,
-                format!("Key '{}' does not exist", key),
-            ));
-        }
-
-        let command = LogEntry::Rm(key.clone());
-        bincode::serialize_into(&mut self.writer, &command)?;
-        self.writer.flush()?;
-
-        if let Some(prev_index) = self.index_map.remove(&key) {
-            self.garbage += prev_index.len;
-            if self.garbage > GARBAGE_THRESHOLD {
-                self.merge()?;
-            }
-        };
-        Ok(())
-    }
-
-    fn merge(&mut self) -> Result<()> {
-        // create 2 new log: one for the merged entries and one for the new active log
-        let merged_epoch = self.active_epoch + 1;
-        self.active_epoch += 2;
-        let (writer, reader) = create_log(&self.active_path, self.active_epoch)?;
-        self.writer = writer;
-        let active_epoch = self.active_epoch;
-        self.readers.insert(active_epoch, reader);
-        let (mut merged_writer, merged_reader) = create_log(&self.active_path, merged_epoch)?;
-        self.readers.insert(merged_epoch, merged_reader);
-
-        let mut new_index_map = self.index_map.clone();
-        // copy data from old log files to the merged log file and update the in-memory index map
-        for index in new_index_map.values_mut() {
-            match self.readers.get_mut(&index.gen) {
-                Some(reader) => {
-                    reader.seek(SeekFrom::Start(index.pos))?;
-                    let mut entry_reader = reader.take(index.len);
-
-                    let merged_pos = merged_writer.pos;
-                    io::copy(&mut entry_reader, &mut merged_writer)?;
-
-                    *index = LogIndex {
-                        gen: merged_epoch,
-                        pos: merged_pos,
-                        len: index.len,
-                    };
-                }
-                None => {
-                    return Err(Error::new(
-                        ErrorKind::CorruptedIndex,
-                        format!("Could not get reader for epoch #{}", index.gen),
-                    ))
-                }
-            }
-        }
-        self.index_map.clear();
-        self.index_map.clone_from(&new_index_map);
-        merged_writer.flush()?;
-
-        // remove stale log files
-        let stale_epochs: Vec<u64> = self
-            .readers
-            .keys()
-            .filter(|&&epoch| epoch < merged_epoch)
-            .cloned()
-            .collect();
-        for epoch in stale_epochs {
-            let log_path = self.active_path.join(format!("epoch-{}.log", epoch));
-            fs::remove_file(log_path)?;
-            self.readers.remove(&epoch);
-        }
-        self.garbage = 0;
-        Ok(())
+        todo!("new context types")
     }
 }
 
 /// A database's writer that updates on-disk files and maintains consistent index to those files
+#[derive(Debug)]
 struct WriteContext {
     path: Arc<PathBuf>,
     index: Arc<RwLock<BTreeMap<String, LogIndex>>>,
     writer: BufSeekWriter<File>,
-    epoch: u64,
+    gen: u64,
     garbage: u64,
 }
 
 impl WriteContext {
     fn set(&mut self, key: String, val: String) -> Result<()> {
-        /*
-            When setting a value a key, a `Set` command is written to disk in a sequential log,
-            then the log pointer (file position) is stored in an in-memory index from key to pointer.
-        */
         let mut index = self.index.write().unwrap();
 
         let pos = self.writer.pos;
-        let command = LogEntry::Set(key.clone(), val);
-        bincode::serialize_into(&mut self.writer, &command)?;
+        let log_entry = LogEntry::Set(key.clone(), val);
+        bincode::serialize_into(&mut self.writer, &log_entry)?;
         self.writer.flush()?;
+        let len = self.writer.pos - pos;
 
         let log_entry = LogIndex {
-            gen: self.epoch,
+            gen: self.gen,
             pos,
-            len: self.writer.pos - pos,
+            len,
         };
         if let Some(prev_log_entry) = index.insert(key, log_entry) {
             self.garbage += prev_log_entry.len;
@@ -331,12 +193,6 @@ impl WriteContext {
     }
 
     fn remove(&mut self, key: String) -> Result<()> {
-        /*
-            When removing a key, the store searches for the key in the index. If an index is found,
-            a `Remove` command is written to disk a in sequential log, and the key is removed from
-            the in-memory index.
-        */
-
         let mut index = self.index.write().unwrap();
 
         if !index.contains_key(&key) {
@@ -346,8 +202,8 @@ impl WriteContext {
             ));
         }
 
-        let command = LogEntry::Rm(key.clone());
-        bincode::serialize_into(&mut self.writer, &command)?;
+        let log_entry = LogEntry::Rm(key.clone());
+        bincode::serialize_into(&mut self.writer, &log_entry)?;
         self.writer.flush()?;
 
         if let Some(prev_index) = index.remove(&key) {
@@ -360,11 +216,10 @@ impl WriteContext {
     }
 
     fn merge(&mut self) -> Result<()> {
-        todo!()
-        //    // create 2 new log: one for the merged entries and one for the new active log
-        //    let merged_epoch = self.epoch + 1;
-        //    let new_epoch = self.epoch + 2;
-        //    self.epoch += 2;
+        let merged_gen = self.gen + 1;
+        let new_gen = self.gen + 2;
+
+        Ok(())
 
         //    let (writer, reader) = create_log(self.path.as_ref(), self.epoch)?;
         //    let epoch = self.epoch;
@@ -417,39 +272,53 @@ impl WriteContext {
 }
 
 /// A database's reader that reads from on-disk files based on the current index
+#[derive(Debug)]
 struct ReadContext {
     path: Arc<PathBuf>,
     index: Arc<RwLock<BTreeMap<String, LogIndex>>>,
-    readers: BTreeMap<u64, BufSeekReader<File>>,
+    readers: RefCell<BTreeMap<u64, BufSeekReader<File>>>,
+}
+
+impl Clone for ReadContext {
+    fn clone(&self) -> Self {
+        // The `ReadContext` will be cloned and sent across threads. Each cloned `ReadContext`
+        // will have unique file handles to the log files so that read can happen concurrently
+        Self {
+            path: Arc::clone(&self.path),
+            index: Arc::clone(&self.index),
+            readers: RefCell::new(BTreeMap::new()),
+        }
+    }
 }
 
 impl ReadContext {
-    fn get(&mut self, key: String) -> Result<Option<String>> {
-        /*
-            When retrieving a value for a key, the store searches for the key in the index. If
-            found an index, loads log entry at the corresponding log pointer, evaluates the command,
-            and returns the result.
-        */
-        let index = self.index.read().unwrap();
-        let get_result = index.get(&key).cloned();
-        match get_result {
-            Some(index) => match self.readers.get_mut(&index.gen) {
-                Some(reader) => {
-                    reader.seek(SeekFrom::Start(index.pos))?;
-                    match bincode::deserialize_from(reader)? {
-                        LogEntry::Set(_, value) => Ok(Some(value)),
-                        _ => Err(Error::new(
-                            ErrorKind::CorruptedLog,
-                            "Expecting a log entry for a set operation",
-                        )),
-                    }
-                }
-                None => Err(Error::new(
-                    ErrorKind::CorruptedIndex,
-                    format!("Could not get reader for epoch #{}", index.gen),
-                )),
-            },
+    fn get(&self, key: String) -> Result<Option<String>> {
+        let res = {
+            let index = self.index.read().unwrap();
+            index.get(&key).cloned()
+        };
+
+        match res {
             None => Ok(None),
+            Some(index) => {
+                let log_entry = {
+                    let mut readers = self.readers.borrow_mut();
+                    let reader = readers
+                        .entry(index.gen)
+                        .or_insert(open_log(self.path.as_ref(), index.gen)?);
+
+                    reader.seek(SeekFrom::Start(index.pos))?;
+                    bincode::deserialize_from(reader)?
+                };
+
+                match log_entry {
+                    LogEntry::Set(_, value) => Ok(Some(value)),
+                    _ => Err(Error::new(
+                        ErrorKind::CorruptedLog,
+                        "Expecting a log entry for a set operation",
+                    )),
+                }
+            }
         }
     }
 }
@@ -470,7 +339,7 @@ struct LogIndex {
 fn build_index(
     reader: &mut BufSeekReader<File>,
     index_map: &mut BTreeMap<String, LogIndex>,
-    epoch: u64,
+    gen: u64,
 ) -> Result<u64> {
     reader.seek(SeekFrom::Start(0))?;
     let mut garbage = 0;
@@ -479,11 +348,8 @@ fn build_index(
         match bincode::deserialize_from(reader.by_ref()) {
             Ok(e) => match e {
                 LogEntry::Set(key, _) => {
-                    let index = LogIndex {
-                        gen: epoch,
-                        pos,
-                        len: reader.pos - pos,
-                    };
+                    let len = reader.pos - pos;
+                    let index = LogIndex { gen, pos, len };
                     if let Some(prev_index) = index_map.insert(key, index) {
                         garbage += prev_index.len;
                     };
@@ -496,6 +362,7 @@ fn build_index(
             },
             Err(err) => match err.as_ref() {
                 bincode::ErrorKind::Io(io_err) => match io_err.kind() {
+                    // TODO: Note down why this is ok
                     io::ErrorKind::UnexpectedEof => break,
                     _ => return Err(Error::from(err)),
                 },
@@ -506,11 +373,21 @@ fn build_index(
     Ok(garbage)
 }
 
-fn create_log<P>(path: P, epoch: u64) -> Result<(BufSeekWriter<File>, BufSeekReader<File>)>
+fn open_log<P>(path: P, gen: u64) -> Result<BufSeekReader<File>>
 where
     P: AsRef<Path>,
 {
-    let log_path = path.as_ref().join(format!("epoch-{}.log", epoch));
+    let log_path = path.as_ref().join(format!("gen-{}.log", gen));
+    let readable_log = OpenOptions::new().read(true).open(&log_path)?;
+    let reader = BufSeekReader::new(readable_log)?;
+    Ok(reader)
+}
+
+fn create_log<P>(path: P, gen: u64) -> Result<(BufSeekWriter<File>, BufSeekReader<File>)>
+where
+    P: AsRef<Path>,
+{
+    let log_path = path.as_ref().join(format!("gen-{}.log", gen));
 
     let writable_log = OpenOptions::new()
         .create_new(true)
@@ -523,25 +400,25 @@ where
     Ok((writer, reader))
 }
 
-fn previous_epochs<P>(path: P) -> Result<Vec<u64>>
+fn previous_gens<P>(path: P) -> Result<Vec<u64>>
 where
     P: AsRef<Path>,
 {
-    let mut epochs: Vec<u64> = fs::read_dir(&path)?
+    let mut gens: Vec<u64> = fs::read_dir(&path)?
         .filter_map(std::result::Result::ok)
         .map(|e| e.path())
         .filter(|p| p.is_file() && p.extension() == Some("log".as_ref()))
         .filter_map(|p| {
             p.file_stem()
                 .and_then(OsStr::to_str)
-                .filter(|s| s.starts_with("epoch-"))
-                .map(|s| s.trim_start_matches("epoch-"))
+                .filter(|s| s.starts_with("gen-"))
+                .map(|s| s.trim_start_matches("gen-"))
                 .map(str::parse::<u64>)
         })
         .filter_map(std::result::Result::ok)
         .collect();
-    epochs.sort();
-    Ok(epochs)
+    gens.sort();
+    Ok(gens)
 }
 
 #[derive(Debug)]
